b05134dff692f7505612cb2c5e6e329dff2998c6
Eric Ung <ungericw@tutanota.com>
2021-09-19 15:27:37 -0500

    Add WordFrequencies

---
+++ Moderate/Problem01/WordFrequencies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moderate.Problem01
+{
+    public class WordFrequencies
+    {
+        private Dictionary<string, int> _frequency = new Dictionary<string, int>();
+
+        public void InitializeBook(string book)
+        {
+            foreach(string word in book.Split(' '))
+            {
+                if (_frequency.ContainsKey(word))
+                {
+                    _frequency[word]++;
+                }
+                else
+                {
+                    _frequency[word] = 1;
+                }
+            }
+        }
+
+        public int Count(string word)
+        {
+            return _frequency.ContainsKey(word) ? _frequency[word] : 0;
+        }
+    }
+}
