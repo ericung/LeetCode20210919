b2e58791ef5e12712209ff7e9a90b2f10585c812
ungericw <ungericw@tutanota.com>
2021-10-02 14:31:58 -0500

    Add sketch of Problem04

---
+++ Moderate/Problem04/TicTacWin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moderate.Problem04
+{
+    public class TicTacWin
+    {
+
+    }
+}
---
+++ ModerateTests/Problem04/TicTacWinTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModerateTests.Problem04
+{
+    [TestClass]
+    public class TicTacWinTests
+    {
+    }
+}
