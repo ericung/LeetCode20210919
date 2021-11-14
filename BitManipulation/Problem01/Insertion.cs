191e44ee39654889288d3d744269cb3d7f17c018
ungericw <ungericw@tutanota.com>
2021-11-14 11:10:57 -0600

    Add BitManipulation project

---
+++ BitManipulation/Problem01/Insertion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitManipulation.Problem01
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Insertion
+    {
+        public ListNode RemoveElements(ListNode head, int val) {
+            if (head is null)
+            {
+                return null;
+            }
+        
+            ListNode newHead = head;
+            ListNode prevNode = newHead;
+            ListNode node;
+        
+            while(prevNode is object)
+            {
+                Console.WriteLine(prevNode.val);
+                if (prevNode.val == val)
+                {
+                    prevNode = prevNode.next;
+                }
+
+                if (prevNode is null)
+                {
+                    Console.WriteLine("null!");
+                }
+            }
+        
+            newHead = prevNode;
+        
+            if (prevNode.next is object)
+            {
+                node = prevNode.next;
+                while(node is object)
+                {
+                    if (node.val != val)
+                    {
+                        prevNode = node;
+                        node = node.next;
+                    }
+                    else
+                    {
+                        prevNode = node.next;
+                        node = node.next;
+                    }
+                }
+            }
+        
+            return newHead;
+        }
+    }
+}
---
+++ BitManipulationTests/Problem01/InsertionTests.cs
@@ -0,0 +1,20 @@
+﻿using BitManipulation.Problem01;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BitManipulationTests.Problem01
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Insertion insertion = new Insertion();
+            ListNode head = new ListNode(6, new ListNode(6, new ListNode(6, new ListNode(6))));
+
+            ListNode newNode = insertion.RemoveElements(head, 6);
+        }
+    }
+}
